e47189d1e1befb985db5e9c9cb99cdcc0deaa3d0
ijafri <irene.jafri@oicr.on.ca>
2019-06-11 14:42:23 -0400

    Annotations added for review

--- bcl2fastq/index_summary.py
+++ bcl2fastq/index_summary.py
@@ -6,15 +6,18 @@ import dash.dependencies as dep
 import pandas
 
 
+
 index = gsiqcetl.bcl2fastq.parse.load_cache(
     gsiqcetl.bcl2fastq.parse.CACHENAME.SAMPLES,
     './data/bcl2fastq_cache.hd5'
 )
-#set variable index(referring to all indices of the run) to data file in module 
+ 
 unknown = gsiqcetl.bcl2fastq.parse.load_cache(
     gsiqcetl.bcl2fastq.parse.CACHENAME.UNKNOWN,
     './data/bcl2fastq_cache.hd5'
 )
+
+
 
 all_runs = index['Run'].sort_values(ascending=False).unique()
 all_runs = [{'label': r, 'value': r} for r in all_runs]
@@ -25,9 +28,14 @@ layout = html.Div(children=[
         options=all_runs,
         value=all_runs[0]['value'],
     ),
+#           Dropdown option's label set to all_runs(combination of unknown and known)
+#           Dropdown multi set to false (default) - only single select possible 
+#                 -  multiple select gives error due to inconsistent values 
     dcc.Graph(
         id='known_index_bar',
+
     ),
+            #Bar graph with defaults set, no change in colour etc
     html.Div([
         html.Div(
             [dcc.Graph(id='known_unknown_pie'),
@@ -40,31 +48,43 @@ layout = html.Div(children=[
                      "or unknown. This is due to multiple bcl2fastq analyses "
                      "being used on one run. This number should be 100%."
                  ),
+                        #This is the textbox at the bottom, hover over to see title
              )],
-            style={'width': '24%', 'display': 'inline-block'}
+            style={'width': '24%', 'display': 'inline-block', }
         ),
+                 #Pie chart, by default is set to left with a width of 24% of secreen. 
         html.Div(
             [dcc.Graph(id='unknown_index_bar')],
-            style={'width': '74%', 'display': 'inline-block', 'float': 'right'}
+            style={'width': '76%', 'display': 'inline-block', 'float': 'center'}
         ),
     ]),
     html.Div(id='sample_run_hidden', style={'display': 'none'}),
     html.Div(id='pruned_unknown_hidden', style={'display': 'none'}),
+            #html.div is just a way of dividing the HTML into meaningful groups
+            #Within the main layout division there are 4 groups: 
+            #           1. the section with the 100% bar graph
+            #           2.  Nested subdivision of: 
+            #                   a) pie graph at 24%
+            #                   b) bar graph at 76%
+            #           3. Sample Run hidden
+            #           4. Pruned  Unknown Hidden
+            #Note: span can be used for inline setting
 ])
 
 try:
-    from app import app
-except ModuleNotFoundError:
+    from app import app #From package app import app in which case the first instance of the word is the package
+except ModuleNotFoundError: #When module not found, terminate
     import dash
-    app = dash.Dash(__name__)
+    app = dash.Dash(__name__) 
     app.layout = layout
 
 
 @app.callback(
     dep.Output('sample_run_hidden', 'children'),
     [dep.Input('run_select', 'value')]
+#           Whenever input(drop down) changes, app.callback re-runs and updates     
 )
-def update_sample_run_hidden(run_alias):
+def update_sample_run_hidden(run_alias):        #Unsure
     run = index[index['Run'] == run_alias]
     run = run[run['ReadNumber'] == 1]
     run = run[~run['SampleID'].isna()]
@@ -89,14 +109,17 @@ def update_pruned_unknown_hidden(run_alias):
 )
 def update_known_index_bar(run_json):
     run = pandas.read_json(run_json, orient='split')
-
-    run['library'] = run['SampleID'].str.extract(
-        'SWID_\d+_(\w+_\d+_.*_\d+_[A-Z]{2})_'
+        # convert a Json string into a panda object where orient is the expected format of json 
+    run['library'] = run['SampleID'].str.extract(   # create DataFrame via str.extract 
+        'SWID_\d+_(\w+_\d+_.*_\d+_[A-Z]{2})_' #this parameter of the column that will be creted
     )
+        #replace index library with index sampleID as a string
     run['index'] = run['Index1'].str.cat(
-        run['Index2'].fillna(''), sep=' '
+        run['Index2'].fillna(''), sep=' ' #concatenate the strings with a space in the middle
     )
-
+    run = run.sort_values('SampleNumberReads', ascending=False)
+    
+    
     data = []
     for inx, d in run.groupby('index'):
         data.append({
@@ -107,10 +130,10 @@ def update_known_index_bar(run_json):
             'name': inx,
             'marker': {'line': {
                 'width': 1,
-                'color': 'rgb(0, 0, 0)'
+                'color': 'rgb(255,255, 255)'
             }}
         })
-
+       
     return {
         'data': data,
         'layout': {
@@ -132,7 +155,7 @@ def update_unknown_index_pie(run_json):
         pruned['Index2'].fillna(''), sep=' '
     )
     pruned = pruned.sort_values('Count', ascending=False)
-    pruned = pruned.head(30)
+    pruned = pruned.head(30) # Amount of data visibile in graph, largest 30 shown
 
     data = []
     for lane, d in pruned.groupby('LaneNumber'):
