f7920236dd0cc8ccb236ee946693359e752b7849
Savo Lazic <savo.lazic@oicr.on.ca>
2019-08-01 15:13:10 -0400

    Created setup.py for easy and consistent installs

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='dashi',
+   version='0.0.1',
+   description='Visualizer for GSI QC data',
+   author='Savo Lazic',
+   author_email='savo.lazic@oicr.on.ca',
+   python_requires='>=3.6.0',
+   packages=find_packages(),
+   install_requires=[
+      'dash==1.0.2',
+      'plotly==4.0.0',
+      'scipy==1.2.0',
+      'pandas==0.23.4',
+      'tables==3.5.1',
+      'numpy==1.15.4',
+      'gsiqcetl @ git+https://bitbucket.oicr.on.ca/scm/gsi/gsi-qc-etl.git@master'
+   ],
+)
