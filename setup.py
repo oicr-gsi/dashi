ca25ddc322faab62e850bcfce5d808f6afe76a9d
Savo Lazic <savo.lazic@oicr.on.ca>
2019-08-01 15:13:10 -0400

    Created setup.py for easy and consistent installs

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
    name='dashi',
+<<<<<<< HEAD
    version='0.1.0',
+=======
+   version='0.0.1',
+>>>>>>> Created setup.py for easy and consistent installs
    description='Visualizer for GSI QC data',
    author='Savo Lazic',
    author_email='savo.lazic@oicr.on.ca',
@@ -15,7 +19,10 @@ setup(
       'pandas==0.23.4',
       'tables==3.5.1',
       'numpy==1.15.4',
+<<<<<<< HEAD
       'sd-material-ui==3.1.2',
+=======
+>>>>>>> Created setup.py for easy and consistent installs
       'gsiqcetl @ git+https://bitbucket.oicr.on.ca/scm/gsi/gsi-qc-etl.git@master'
    ],
 )
