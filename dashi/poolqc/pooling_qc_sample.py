b0e6a9d9dbde45d00ce9bce8fc75b5edcd07fbd5
Savo Lazic <savo.lazic@oicr.on.ca>
2019-08-01 13:20:36 -0400

    Moved code files into dashi folder to allow for setup.py implementation

--- dashi/poolqc/pooling_qc_sample.py
+++ dashi/poolqc/pooling_qc_sample.py
@@ -2,6 +2,7 @@ import gsiqcetl.bcl2fastq.parse
 import gsiqcetl.bcl2fastq.utility
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import dash
 >>>>>>> 0673ad8... Update of Layout:
@@ -9,6 +10,8 @@ import dash
 import dash
 import dash_bootstrap_components as dbc
 >>>>>>> 82eb00e... intermediate file
+=======
+>>>>>>> 64ed902... Moved code files into dashi folder to allow for setup.py implementation
 import dash_core_components as dcc
 import dash_html_components as html
 import dash.dependencies as dep
@@ -20,6 +23,7 @@ import urllib
 from datetime import datetime
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import sd_material_ui as sd
 
@@ -27,6 +31,8 @@ import sd_material_ui as sd
 =======
 import sd_material_ui as sd
 >>>>>>> d0a2e74... intermediate commit
+=======
+>>>>>>> 64ed902... Moved code files into dashi folder to allow for setup.py implementation
 
 pandas.options.mode.chained_assignment = None
 
@@ -42,6 +48,7 @@ bcl2fastq['library'] = bcl2fastq['SampleID'].str.extract('SWID_\d+_(\w+_\d+_.*_\
 
 df = bcl2fastq.merge(rnaseq, on='library', how='outer')
 df = df.merge(bamqc, on='library', how='outer')
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 df['properly paired reads'] = np.where(df['properly paired reads'].isnull(),
@@ -134,6 +141,9 @@ layout = html.Div(children=[
 =======
 =======
 >>>>>>> d0a2e74... intermediate commit
+=======
+
+>>>>>>> 64ed902... Moved code files into dashi folder to allow for setup.py implementation
 df = df.drop(columns=['Sequencer Run Name', 'Lane Number'])
 
 runs = df['Run'].sort_values(ascending=False).unique()
@@ -193,6 +203,7 @@ navbar = dbc.NavbarSimple(
 )
 >>>>>>> 82eb00e... intermediate file
 layout = html.Div(children=[
+<<<<<<< HEAD
 <<<<<<< HEAD
     dcc.ConfirmDialog(
         id='warning',
@@ -261,11 +272,67 @@ layout = html.Div(children=[
                 style_header={'backgroundColor': 'rgb(222,222,222)',
                               'fontSize': 16,
                               'fontWeight': 'bold'},
+=======
+    dcc.ConfirmDialog(
+        id='warning',
+        message='The selected run does not return any data. Analysis may have not been completed yet.' '''
+>>>>>>> 64ed902... Moved code files into dashi folder to allow for setup.py implementation
 
-            )
-        ),
-    ])
-])
+        '''' Click either "Ok" or "Cancel" to return to the most recent run.'
+    ),
+    dcc.Location(
+        id='run_url',
+        refresh=False
+    ),
+    html.Div(children=
+    dcc.Dropdown(
+        id='select_a_run',
+        options=[{'label': r, 'value': r} for r in runs],
+        value=runs[0],
+        clearable=False
+    )),
+    html.Div(
+        dcc.Dropdown(
+            id='lane_select'
+        )),
+    html.Div(id='Title', style={'fontSize': 25, 'textAlign': 'center', 'padding': 30}),
+    html.Div(children=''),
+    html.A(
+        'Download Data',
+        id='download-link',
+        download='rawdata.csv',
+        href='',
+        target='_blank'
+    ),
+    html.Div(
+        dt.DataTable(
+            id='Summary Table',
+            editable=True,
+            row_selectable='multi',
+            selected_rows=[],
+            style_cell={
+                'minWidth': '150px',
+                'textAlign': 'center'
+            },
+            style_table={
+                'maxHeight': '1000px',
+                'maxWidth': '100%',
+                'overflowY': 'scroll',
+                'overflowX': 'scroll'
+            },
+            style_header={'backgroundColor': 'rgb(222,222,222)',
+                          'fontSize': 16,
+                          'fontWeight': 'bold'},
+
+        )),
+    html.Div(
+        dcc.Graph(id='SampleIndices'),
+    ),
+    html.Div(
+        dcc.Graph(id='map_code')
+    )
+]
+)
 
 try:
     from app import app
@@ -318,15 +385,18 @@ def update_title(lane_value, run_value):
 
 
 @app.callback(
-    dep.Output('Filter_drawer', 'open'),
-    [dep.Input('filters', 'n_clicks')]
+    [dep.Output('Summary Table', 'columns'),
+     dep.Output('Summary Table', 'data'),
+     dep.Output('Summary Table', 'style_data_conditional'),
+     dep.Output('download-link', 'href'),
+     dep.Output('download-link', 'download')],
+    [dep.Input('select_a_run', 'value'),
+     dep.Input('lane_select', 'value')]
 )
-def open_project_drawer(n_clicks):
-    return n_clicks is not None
-
-
-def Summary_table(run):
-    # Adding 'on-the-fly' metrics
+def Summary_table(run_alias, lane_alias):
+    run = df[(df['Run'] == run_alias) & (df['LaneNumber'] == lane_alias)]
+    run = run[~run['Run'].isna()].drop_duplicates('library')
+    run = run[~run['library'].isna()]
     run['% Mapped to Coding'] = run['Coding Bases'] / run['Passed Filter Aligned Bases'] * 100
     run['% Mapped to Intronic'] = run['Intronic Bases'] / run['Passed Filter Aligned Bases'] * 100
     run['% Mapped to Intergenic'] = run['Intergenic Bases'] / run['Passed Filter Aligned Bases'] * 100
@@ -334,58 +404,78 @@ def Summary_table(run):
 
     run = run.round(2)
     run = run.filter(
-        ['library', 'Run', 'LaneNumber', 'Index1', 'Index2', 'SampleNumberReads', '% Mapped to Coding',
-         '% Mapped to Intronic',
-         '% Mapped to Intergenic', 'rRNA Contamination (%reads aligned)', 'Proportion Correct Strand Reads',
-         'Coverage'])
-    run = run.sort_values('library')
+        ['library', 'Run', 'LaneNumber', 'Index1', 'Index2', 'YieldQ30', '% Mapped to Coding', '% Mapped to Intronic',
+         '% Mapped to Intergenic',
+         'rRNA Contamination (%reads aligned)', 'Proportion Correct Strand Reads', 'Coverage'])
+
     csv = run.to_csv(index=False)
     csv = 'data:text/csv;charset=utf-8,' + urllib.parse.quote(csv)
     run = run.drop(columns=['Run', 'LaneNumber'])
-
-    # Append Pass/Fail and Thresholds
 
     columns = [{'name': i, 'id': i,
                 'type': 'numeric'} for i in run.columns]
 
     data = run.to_dict('records')
 
-    # highlighting datatable cells/columns/rows
     style_data_conditional = [{
         'if': {'column_id': 'library'},
         'backgroundColor': 'rgb(222, 222, 222)'
-    }, {'if': {'column_id': '% Mapped to Coding',
-               'filter_query': '0 < {% Mapped to Coding} and {% Mapped to Coding} < 20'},
-        'backgroundColor': 'rgb(219, 75, 75)'},
+    },
+        {'if': {'column_id': '% Mapped to Coding',
+                'filter_query': '0 < {% Mapped to Coding} < 20'},
+         'backgroundColor': 'rgb(219, 75, 75)'},
         {'if': {'column_id': '% Mapped to Intronic',
-                'filter_query': '0 < {% Mapped to Intronic} and {% Mapped to Intronic} < 20'},
+                'filter_query': '0 < {% Mapped to Coding} < 20'},
          'backgroundColor': 'rgb(219, 75, 75)'},
         {'if': {'column_id': '% Mapped to Intergenic',
-                'filter_query': '0 < {% Mapped to Intergenic} and {% Mapped to Intergenic} < 15'},
-         'backgroundColor': 'rgb(219, 75, 75)'},
-        {'if': {'column_id': 'rRNA Contamination (%reads aligned)',
-                'filter_query': '0 < {rRNA Contamination (%reads aligned)} and {rRNA Contamination (%reads aligned)} < 15'},
+                'filter_query': '0 < {% Mapped to Intergenic} < 15'},
          'backgroundColor': 'rgb(219, 75, 75)'},
     ]
-    return columns, data, style_data_conditional, csv
+    downloadtimedate = datetime.today().strftime('%Y-%m-%d')
+    download = 'PoolQC_%s_%s_%s.csv' % (downloadtimedate, run_alias, lane_alias)
+
+    return columns, data, style_data_conditional, csv, download
 
 
-def update_sampleindices(run):
-    run = run.sort_values('library')
-    num_libraries = len(run['library'])
-    samples_passing_clusters = '%s/%s' % (sum(i > 20000 for i in run['SampleNumberReads']), num_libraries)
-    index_threshold = 50000000
+def update_sampleindices(run, rows, derived_virtual_selected_rows, colors):
     data = []
 
-    for inx, d in run.groupby(['library']):
-        d['Threshold'] = index_threshold
-        d['Color'] = np.where((d['SampleNumberReads'] >= index_threshold), '#ffffff', '#db4b4b')
+    for inx, d in run.groupby(['index']):
         data.append(
             go.Bar(
                 x=list(d['library']),
                 y=list(d['SampleNumberReads']),
                 name=inx,
-                marker={'color': '#20639B',
+                marker={'color': colors},
+            )
+        )
+
+    return {
+        'data': data,
+        'layout': {
+            'title': 'Index Clusters per Sample ',
+            'xaxis': {'title': 'Sample', 'automargin': True},
+            'yaxis': {'title': 'Clusters'},
+
+        }
+    }
+
+
+def update_percent_mapped_to_code(run, rows, derived_virtual_selected_rows, colors):
+    run = run[~run['rRNA Contamination (%reads aligned)'].isna()]
+    run = run.sort_values(by='Result', ascending=False)
+    data = []
+
+    for inx, d in run.groupby(['LaneNumber']):
+        d['Threshold'] = 20
+        d['Color'] = np.where((d['Result'] >= d['Threshold']), '#ffffff', '#db4b4b')
+        data.append(
+            go.Bar(
+                y=list(d['library']),
+                x=list(d['Result']),
+                orientation='h',
+                name=inx,
+                marker={'color': colors,
                         'line': {
                             'width': 3,
                             'color': list(d['Color'])
@@ -394,48 +484,55 @@ def update_sampleindices(run):
         )
         data.append(
             go.Scatter(
-                x=list(d['library']),
-                y=list(d['Threshold']),
-                mode='markers+lines',
+                y=list(d['library']),
+                x=list(d['Threshold']),
+                orientation='h',
+                name=inx,
                 line={
                     'width': 3,
-                    'color': 'rgb(0,0,0)',
+                    'color': 'rgb(204,0,0)',
                     'dash': 'dash',
-                }, ))
+                },
+                mode='lines'))
 
     return {
         'data': data,
         'layout': {
-            'title': 'Index Clusters per Sample. Passed Samples: %s Threshold: %s' % (
-                samples_passing_clusters, index_threshold),
-            'xaxis': {'title': 'Sample', 'automargin': True},
-            'yaxis': {'title': 'Clusters'},
+            'title': 'Per Cent Mapped to Coding (RNA) ',
+            'yaxis': {'title': 'Sample', 'automargin': True},
+            'xaxis': {'title': 'Per Cent', 'range': [0, 100]},
             'showlegend': False,
-
+            'height': (len(run) * 30) + 150
         }
     }
 
 
 @app.callback(
-    [dep.Output('SampleIndices', 'figure'),
-     dep.Output('Summary Table', 'columns'),
-     dep.Output('Summary Table', 'data'),
-     dep.Output('Summary Table', 'style_data_conditional'),
-     dep.Output('download-link', 'href'),
-     dep.Output('download-link', 'download')],
+    [dep.Output('map_code', 'figure'),
+     dep.Output('SampleIndices', 'figure')],
     [dep.Input('select_a_run', 'value'),
-     dep.Input('lane_select', 'value')])
-def update_graphs(run_alias, lane_alias):
+     dep.Input('lane_select', 'value'),
+     dep.Input('Summary Table', 'derived_virtual_data'),
+     dep.Input('Summary Table', 'derived_virtual_selected_rows')])
+def update_all_graphs(run_alias, lane_alias, rows, derived_virtual_selected_rows):
     run = df[(df['Run'] == run_alias) & (df['LaneNumber'] == lane_alias)]
     run = run[~run['Run'].isna()].drop_duplicates('library')
-    run = run[~run['library'].isna()]
+    run['Result'] = run['Coding Bases'] / run['Passed Filter Aligned Bases'] * 100
+    run['index'] = run['Index1'].str.cat(
+        run['Index2'].fillna(''), sep=' ')
 
-    downloadtimedate = datetime.today().strftime('%Y-%m-%d')
-    download = 'PoolQC_%s_%s_%s.csv' % (downloadtimedate, run_alias, lane_alias)
+    if derived_virtual_selected_rows is None:
+        derived_virtual_selected_rows = []
 
-    columns, data, style_data_conditional, csv = Summary_table(run)
+    dff = run if rows is None else pandas.DataFrame(rows)
 
-    return update_sampleindices(run), columns, data, style_data_conditional, csv, download
+    colors = ['#d9c76c' if i in derived_virtual_selected_rows else '#99b2c2'
+              for i in range(len(dff))]
+
+    return update_percent_mapped_to_code(run, rows, derived_virtual_selected_rows, colors), update_sampleindices(run,
+                                                                                                                 rows,
+                                                                                                                 derived_virtual_selected_rows,
+                                                                                                                 colors)
 
 
 if __name__ == '__main__':
