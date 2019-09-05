f87dd16e6ff00f2d8db322143755c6467177b9f8
Savo Lazic <savo.lazic@oicr.on.ca>
2019-09-05 15:24:16 -0400

    Update Changelog and setup.py for new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='dashi',
-   version='0.1.0',
+   version='0.2.0',
    description='Visualizer for GSI QC data',
    author='Savo Lazic',
    author_email='savo.lazic@oicr.on.ca',
