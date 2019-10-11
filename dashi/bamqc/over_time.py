b39935e5c8b02ed6ef72e070fea2707bcbc79271
Savo Lazic <savo.lazic@oicr.on.ca>
2019-10-11 11:13:53 -0400

    Renamed global variables for clarity

--- dashi/bamqc/over_time.py
+++ dashi/bamqc/over_time.py
@@ -10,8 +10,8 @@ from gsiqcetl.pinery.sampleprovenance.constants import (
 from dashi.plots.shiny_mimic import ShinyMimic
 
 
-RNA_DF = gsiqcetl.load.bamqc(CacheSchema.v1)
-RNA_COL = gsiqcetl.load.bamqc_columns(CacheSchema.v1)
+BAMQC_DF = gsiqcetl.load.bamqc(CacheSchema.v1)
+BAMQC_COL = gsiqcetl.load.bamqc_columns(CacheSchema.v1)
 
 COL_RUN_DATE = "Run Date"
 PROJECT = "Project"
@@ -20,23 +20,25 @@ FRACTION_MAPPED = "Read Fraction Mapped"
 FRACTION_SECONDARY = "Read Fraction Secondary"
 
 # The Run Name is used to extract the date
-RNA_DF[COL_RUN_DATE] = (
-    RNA_DF[RNA_COL.Run].dropna().apply(lambda x: x.split("_")[0])
+BAMQC_DF[COL_RUN_DATE] = (
+    BAMQC_DF[BAMQC_COL.Run].dropna().apply(lambda x: x.split("_")[0])
 )
 # Some runs do not have the proper format and will be excluded
-RNA_DF = RNA_DF[RNA_DF[COL_RUN_DATE].str.isnumeric()]
-RNA_DF[COL_RUN_DATE] = pandas.to_datetime(RNA_DF[COL_RUN_DATE], yearfirst=True)
-
-RNA_DF[PROJECT] = RNA_DF[RNA_COL.Library].apply(lambda x: x.split("_")[0])
-
-RNA_DF[FRACTION_ON_TARGET] = (
-    RNA_DF[RNA_COL.ReadsOnTarget] / RNA_DF[RNA_COL.TotalReads]
+BAMQC_DF = BAMQC_DF[BAMQC_DF[COL_RUN_DATE].str.isnumeric()]
+BAMQC_DF[COL_RUN_DATE] = pandas.to_datetime(
+    BAMQC_DF[COL_RUN_DATE], yearfirst=True
 )
-RNA_DF[FRACTION_MAPPED] = (
-    RNA_DF[RNA_COL.MappedReads] / RNA_DF[RNA_COL.TotalReads]
+
+BAMQC_DF[PROJECT] = BAMQC_DF[BAMQC_COL.Library].apply(lambda x: x.split("_")[0])
+
+BAMQC_DF[FRACTION_ON_TARGET] = (
+    BAMQC_DF[BAMQC_COL.ReadsOnTarget] / BAMQC_DF[BAMQC_COL.TotalReads]
 )
-RNA_DF[FRACTION_SECONDARY] = (
-    RNA_DF[RNA_COL.NonPrimaryReads] / RNA_DF[RNA_COL.TotalReads]
+BAMQC_DF[FRACTION_MAPPED] = (
+    BAMQC_DF[BAMQC_COL.MappedReads] / BAMQC_DF[BAMQC_COL.TotalReads]
+)
+BAMQC_DF[FRACTION_SECONDARY] = (
+    BAMQC_DF[BAMQC_COL.NonPrimaryReads] / BAMQC_DF[BAMQC_COL.TotalReads]
 )
 
 # Pull in meta data from Pinery
@@ -61,10 +63,10 @@ PINERY = PINERY[
     ]
 ]
 
-RNA_DF = RNA_DF.merge(
+BAMQC_DF = BAMQC_DF.merge(
     PINERY,
     how="left",
-    left_on=[RNA_COL.Library, RNA_COL.Run, RNA_COL.Lane],
+    left_on=[BAMQC_COL.Library, BAMQC_COL.Run, BAMQC_COL.Lane],
     right_on=[
         PINERY_COL.SampleName,
         PINERY_COL.SequencerRunName,
@@ -73,31 +75,31 @@ RNA_DF = RNA_DF.merge(
 )
 
 # NaN kits need to be changed to a str. Use the existing Unspecified
-RNA_DF = RNA_DF.fillna({PINERY_COL.PrepKit: "Unspecified"})
-RNA_DF = RNA_DF.fillna({PINERY_COL.LibrarySourceTemplateType: "Unknown"})
+BAMQC_DF = BAMQC_DF.fillna({PINERY_COL.PrepKit: "Unspecified"})
+BAMQC_DF = BAMQC_DF.fillna({PINERY_COL.LibrarySourceTemplateType: "Unknown"})
 # NaN Tissue Origin is set to `nn`, which is used by MISO for unknown
-RNA_DF = RNA_DF.fillna({PINERY_COL.TissueOrigin: "nn"})
+BAMQC_DF = BAMQC_DF.fillna({PINERY_COL.TissueOrigin: "nn"})
 # NaN Tissue Type is set to `n`, which is used by MISO for unknown
-RNA_DF = RNA_DF.fillna({PINERY_COL.TissueType: "n"})
-RNA_DF = RNA_DF.fillna({PINERY_COL.TissuePreparation: "Unknown"})
+BAMQC_DF = BAMQC_DF.fillna({PINERY_COL.TissueType: "n"})
+BAMQC_DF = BAMQC_DF.fillna({PINERY_COL.TissuePreparation: "Unknown"})
 
 # Which metrics can be plotted
 METRICS_TO_GRAPH = (
     FRACTION_ON_TARGET,
     FRACTION_MAPPED,
-    RNA_COL.InsertMean,
-    RNA_COL.ReadsPerStartPoint,
-    RNA_COL.TotalReads,
+    BAMQC_COL.InsertMean,
+    BAMQC_COL.ReadsPerStartPoint,
+    BAMQC_COL.TotalReads,
     FRACTION_SECONDARY,
 )
 
 
 # Which columns will the data table always have
 DEFAULT_TABLE_COLUMN = [
-    {"name": "Library", "id": RNA_COL.Library},
+    {"name": "Library", "id": BAMQC_COL.Library},
     {"name": "Project", "id": PROJECT},
-    {"name": "Run", "id": RNA_COL.Run},
-    {"name": "Lane", "id": RNA_COL.Lane},
+    {"name": "Run", "id": BAMQC_COL.Run},
+    {"name": "Lane", "id": BAMQC_COL.Lane},
     {"name": "Kit", "id": PINERY_COL.PrepKit},
     {"name": "Library Design", "id": PINERY_COL.LibrarySourceTemplateType},
     {"name": "Tissue Origin", "id": PINERY_COL.TissueOrigin},
@@ -117,7 +119,7 @@ SHAPE_COLOUR_COLUMN = [
 
 
 plot_creator = ShinyMimic(
-    lambda: RNA_DF,
+    lambda: BAMQC_DF,
     "bamqc_over_time",
     METRICS_TO_GRAPH,
     SHAPE_COLOUR_COLUMN,
@@ -125,7 +127,7 @@ plot_creator = ShinyMimic(
     PROJECT,
     PINERY_COL.PrepKit,
     COL_RUN_DATE,
-    RNA_COL.Library,
+    BAMQC_COL.Library,
 )
 
 
