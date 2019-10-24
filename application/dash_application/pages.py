e8093052b54e88387bcc00cb692dccd1df436f2f
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-10-24 15:09:26 -0400

    Removes example code (persists in example branch)

--- application/dash_application/pages.py
+++ application/dash_application/pages.py
@@ -3,10 +3,6 @@ import importlib
 prefix = 'application.dash_application.'
 
 pagenames = [
-    'dash_multipage_index',
-    'dash_multipage_1',
-    'dash_multipage_2',
-    'dash_multipage_3'
 ]
 
 pages = []
--- application/routes.py
+++ application/routes.py
@@ -6,10 +6,4 @@ import random
 @app.route('/index')
 def index():
     return render_template('index.html',
-            title='Platonic-Dash Demo')
-
-@app.route('/page2')
-@app.metrics.counter('page2_loads', 'Number of times page2 is loaded')
-def page2():
-    return render_template('page2.html',
-            mystery_item=random.choice(['A', 'B', 'C']))
+            title='Platonic-Dash Default Page')
