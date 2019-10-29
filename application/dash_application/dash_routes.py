9ffb4db141c3fe1f806a5086e2a7cf79d3fc3c2f
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-10-29 09:56:50 -0400

    Dead reference

--- application/dash_application/dash_routes.py
+++ application/dash_application/dash_routes.py
@@ -11,8 +11,7 @@ def add_dash(server):
     for page in pages.pages:
         page.init_callbacks(dash_app)
 
-    from . import dash_multipage_index
-    dash_app.layout = dash_multipage_index.layout
+    dash_app.layout = #FIXME
 
     #Return the server object from Dash to overwrite Flask server object
     return dash_app.server
