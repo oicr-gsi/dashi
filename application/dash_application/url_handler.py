d1dd370ae8d67293cc453ad69617ed34fe0dd554
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-11-06 16:35:54 -0500

    make url handler not horrendous

--- application/dash_application/url_handler.py
+++ application/dash_application/url_handler.py
@@ -2,7 +2,9 @@ import dash_html_components as html
 import dash_core_components as core
 from dash.dependencies import Input, Output
 from .dash_id import init_ids
-from . import bcl2fastq, bamqc_gbovertime, poolqc, runreport, runscanner, bamqc_overtime, rnaseqc
+from . import pages
+
+page_name = None
 
 ids = init_ids(['url', 'page-content'])
 
@@ -14,24 +16,12 @@ layout = html.Div([
 def init_callbacks(dash_app):
     dash_app.config.suppress_callback_exceptions = True
 
-    # TODO: use pages.py or sOMETHING
+    # TODO: You can hang dashi and potentially DoS it by going to <dashi-url>/None
     @dash_app.callback(
         Output(ids['page-content'], 'children'),
         [Input(ids['url'], 'pathname')])
     def url_handler(path):
-        if path == '/{0}'.format(bcl2fastq.page_name):
-            return bcl2fastq.layout
-        elif path == '/{0}'.format(bamqc_gbovertime.page_name):
-            return bamqc_gbovertime.layout
-        elif path == '/{0}'.format(poolqc.page_name):
-            return poolqc.layout
-        elif path == '/{0}'.format(runreport.page_name):
-            return runreport.layout
-        elif path == '/{0}'.format(runscanner.page_name):
-            return runscanner.layout
-        elif path == '/{0}'.format(bamqc_overtime.page_name):
-            return bamqc_overtime.layout
-        elif path == '/{0}'.format(rnaseqc.page_name):
-            return rnaseqc.layout
-        else:
-            return '404'
+        for page in pages.pages:
+            if path == '/{0}'.format(page.page_name):
+                return page.layout
+        return '404'
