ae2c1de62ae6da83abdbf230640f21ba6cce0f75
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-11-07 09:18:23 -0500

    setup

--- application/dash_application/pre_exome.py
+++ application/dash_application/pre_exome.py
@@ -3,26 +3,23 @@ import dash_core_components as core
 from dash.dependencies import Input, Output
 from .dash_id import init_ids
 
-##############################################
-### Add file name to the list in pages.py  ###
-### once you're done!					   ###
-##############################################
+page_name = 'preexome'
 
-## Please refer to the Dash documentation for information
-## on Layouts and Callbacks
-
-## Please add all layout element IDs to this array of String
-## Then, use ids['name_of_element'] instead 'name_of_element' in the id 
-## field of your layout elements
 ids = init_ids([])
 
 ## Dash layout object
-layout = html.Div(children='Placeholder') 
+layout = html.Div(className='body',
+    children=[
+        html.Div(className='sidebar'),
+        html.Div(className='graphs'),
+        html.Div(className='terminal-output'),
+        html.Div(className='data-table'),
+    ]) 
 
 ## Please define all callbacks inside this procedure
 def init_callbacks(dash_app):
-    @app.callback(
-        Output(),
-        [Input()])
-    def placeholder_callback(value):
+    # @dash_app.callback(
+    #     Output(),
+    #     [Input()])
+    # def placeholder_callback(value):
         return
