4143faa064ad68325492159516b9ae6a2170c00f
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-12-03 17:43:45 -0500

    Stop trying to change iterator mid loop
    get_shapes_for_values() never failed from this because ALL_SYMBOLS is so
    long. PLOTY_DEFAULT_COLOURS is much shorter and failed with out of
    bounds errors because index couldn't actually be reset.

--- application/dash_application/plot_builder.py
+++ application/dash_application/plot_builder.py
@@ -2,9 +2,10 @@ from typing import List
 
 import pandas
 import plotly.graph_objects as go
-
-# TODO: if this remains necessary i'll be mad. Can we get it from plotly itself?
 from pandas import DataFrame
+import pinery
+
+PINERY_COL = pinery.column.SampleProvenanceColumn
 
 ALL_SYMBOLS = [
     'circle', 'triangle-up', 'square', 'triangle-down', 'pentagon', 'diamond',
@@ -48,6 +49,19 @@ ALL_SYMBOLS = [
     'line-nw-open'
 ]
 
+PLOTLY_DEFAULT_COLOURS=[
+    '#1f77b4',  # muted blue
+    '#ff7f0e',  # safety orange
+    '#2ca02c',  # cooked asparagus green
+    '#d62728',  # brick red
+    '#9467bd',  # muted purple
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf'   # blue-teal
+]
+
 
 def fill_in_shape_col(df: DataFrame, shape_col: str, shape_or_colour_values:
         dict):
@@ -58,6 +72,14 @@ def fill_in_shape_col(df: DataFrame, shape_col: str, shape_or_colour_values:
         shape_col]), axis=1)
     return df
 
+def fill_in_colour_col(df: DataFrame, colour_col: str, shape_or_colour_values:
+        dict):
+    all_colours = get_colours_for_values(shape_or_colour_values[
+                                           colour_col].tolist())
+    # for each row,
+    df['colour'] = df.apply(lambda row: all_colours.get(row[
+        colour_col]), axis=1)
+    return df
 
 # writing a factory may be peak Java poisoning but it might help with all these parameters
 def generate(title_text, sorted_data, x_fn, y_fn, axis_text, colourby, shapeby,
@@ -89,8 +111,7 @@ def generate(title_text, sorted_data, x_fn, y_fn, axis_text, colourby, shapeby,
             )
         )
     traces = []
-    grouped_data = sorted_data.groupby([colourby, shapeby]) #TODO: is this
-    # inefficient?
+    grouped_data = sorted_data.groupby([colourby, shapeby]) #Unfortunately necessary
     if hovertext_type == 'none':
         marker_mode = 'markers'
     else:
@@ -110,14 +131,14 @@ def generate(title_text, sorted_data, x_fn, y_fn, axis_text, colourby, shapeby,
             hovertext=text_content,
             mode=marker_mode,
             marker={
-                "symbol": data['shape']
+                "symbol": data['shape'],
+                "color": data['colour'] # Please note the 'u'
             }
         )
         traces.append(graph)
     if line_y is not None:
         traces.append(go.Scattergl( # Cutoff line
-            x=sorted_data["sampleName"], # TODO: improve this? Should be
-            # PINERY_COL.SampleName once everything uses PINERY_COL
+            x=sorted_data[PINERY_COL.SampleName], 
             y=[line_y] * len(sorted_data),
             mode="lines",
             line={"width": 1, "color": "black", "dash": "dash"},
@@ -142,9 +163,22 @@ def generate(title_text, sorted_data, x_fn, y_fn, axis_text, colourby, shapeby,
 
 def get_shapes_for_values(shapeby: List[str]):
     shape_dict = {}
-    for index, item in enumerate(shapeby):
+    index = 0
+    for item in shapeby:
         # loop back to beginning of symbols list if we run out of symbols
         if index >= len(ALL_SYMBOLS):
-            index = index - len(ALL_SYMBOLS)
+            index = 0
         shape_dict[item] = ALL_SYMBOLS[index]
+        index += 1
     return shape_dict
+
+def get_colours_for_values(colourby: List[str]):
+    colour_dict = {}
+    index = 0
+    for item in colourby:
+        # loop back to beginning of colours list if we run out of symbols
+        if index >= len(PLOTLY_DEFAULT_COLOURS):
+            index = 0
+        colour_dict[item] = PLOTLY_DEFAULT_COLOURS[index]
+        index += 1
+    return colour_dict
