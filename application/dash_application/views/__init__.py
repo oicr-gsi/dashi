ba9b8d068ed73897ae8ed1d08a064f13a1b87c25
Heather Armstrong <heather.armstrong@oicr.on.ca>
2019-12-03 17:34:40 -0500

    Fix links in dropdown menu

--- application/dash_application/views/__init__.py
+++ application/dash_application/views/__init__.py
@@ -6,11 +6,12 @@ def navbar(current):
         children=[
             dbc.DropdownMenu(
                 children=[
-                    dbc.DropdownMenuItem("Exome", href="exome", style={"fontSize": "12pt"}),
-                    dbc.DropdownMenuItem("Pre-Exome", href="preexome", style={"fontSize": "12pt"}),
-                    dbc.DropdownMenuItem("sWGS", href="swgs", style={"fontSize": "12pt"}),
-                    dbc.DropdownMenuItem("Pre-WGS", href="prewgs", style={"fontSize": "12pt"}),
-                    dbc.DropdownMenuItem("CFMEDIP", href="cfmedip", style={"fontSize": "12pt"}),
+                    dbc.DropdownMenuItem("Pre-Exome", href="preqc-exome",
+                                         style={"fontSize": "12pt"}),
+                    dbc.DropdownMenuItem("Pre-WGS", href="preqc-wgs",
+                                         style={"fontSize": "12pt"}),
+                    dbc.DropdownMenuItem("Pre-RNA", href="preqc-rna",
+                                         style={"fontSize": "12pt"})
                 ],
                 nav=True,
                 in_navbar=True,
