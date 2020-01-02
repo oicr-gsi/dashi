4564c3b27ff73815c8861499a58081afbc3f8822
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2020-01-02 10:15:53 -0500

    version needs to be a string for this to work

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,12 @@
 from flask import current_app as app
 from flask import render_template
 import random
+from version import __version__ as version
 
 ## Use flask's server-side rendering to create a page from templates/index.html
 ## The @app.route decoration tells flask to return this content for both http://<root> and http://<root>/index
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+    version=version)
