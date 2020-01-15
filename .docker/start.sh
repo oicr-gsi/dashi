7b84490d0907ed02e07bde52b8f1949ec0b1df36
Heather Armstrong <callunity@users.noreply.github.com>
2020-01-15 16:09:55 -0500

    Allow specifying use of QC-ETL@master (#105)
    GR-994

--- .docker/start.sh
+++ .docker/start.sh
@@ -7,6 +7,11 @@ then
     /dashi/.docker/enter_passphrase $(cat /run/secrets/ssh_passphrase)
 fi
 
+# Optionally rewrite requirements.txt to use untested current ETL version (gsi-qc-etl@master)
+# instead of a tested release version (gsi-qc-etl@<release version>)
+if [ "$USE_BLEEDING_EDGE_ETL" -eq 1 ]; then
+    sed -irn 's/^\(.*\)\/gsi-qc-etl.git@v.*$/\1\/gsi-qc-etl.git@master/ip' requirements.txt
+fi
 
 # mount your private key for bitbucket into the container to allow this command to succeed
 pip install --trusted-host pypi.python.org -r requirements.txt
