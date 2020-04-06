802eba894cc5fea47df0bee57063e296c82ec7d3
Alexis Varsava <17747491+avarsava@users.noreply.github.com>
2020-04-05 20:15:04 -0400

    update pinery url, we will eventually not track this (#157)

--- .docker/start.sh
+++ .docker/start.sh
@@ -16,7 +16,7 @@ fi
 # mount your private key for bitbucket into the container to allow this command to succeed
 pip install --trusted-host pypi.python.org -r requirements.txt
 
-export PINERY_URL='http://seqbio-pinery-prod-www.hpc.oicr.on.ca:8080/pinery-ws-miso/'
+export PINERY_URL='http://pinery.gsi.oicr.on.ca/'
 export MONGO_URL="mongodb://provenance_ro:$(cat /run/secrets/mongo_password)@provenance-mongo-db.gsi.oicr.on.ca:27017/provenance"
 export GSI_QC_ETL_ROOT_DIRECTORY='/cache'
 
