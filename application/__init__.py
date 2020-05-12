a206e3c0ea537ab35402ad80941d40416c3306e3
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-05-12 09:36:37 -0400

    pandas options

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_caching import Cache
 from prometheus_flask_exporter import PrometheusMetrics
+import pandas
 
 ## Set up Flask application, attach extensions, and load configuration
 def create_app(debug=False):
+    # pandas debug options
+    pandas.set_option('display.max_rows', None)
+    pandas.set_option('display.max_columns', None)
+
     # Construct new Flask core
     app = Flask(__name__,
             instance_relative_config=False)
