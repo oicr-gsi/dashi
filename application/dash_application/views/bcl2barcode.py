cea23e6963e6b7f2ebb66057eb1b900919e8998e
Alexis Varsava <alexis.varsava@oicr.on.ca>
2020-06-17 09:24:37 -0400

    don't drop duplicates

--- application/dash_application/views/bcl2barcode.py
+++ application/dash_application/views/bcl2barcode.py
@@ -178,11 +178,9 @@ def init_callbacks(dash_app):
         """
         known_run = known_data_table[known_data_table[bcl2barcode_col.Run] == run_alias]
         known_run = known_run[~known_run[PINERY_COL.SampleProvenanceID].isna()]
-        known_run = known_run.drop_duplicates([PINERY_COL.SampleProvenanceID, bcl2barcode_col.Lane])
         
         unknown_run = unknown_data_table[unknown_data_table[bcl2barcode_col.Run] == run_alias]
         # unknown_run = unknown_run[~unknown_run[PINERY_COL.SampleProvenanceID].isna()]
-        # unknown_run = unknown_run.drop_duplicates([bcl2barcode_col.SampleProvenanceID, bcl2barcode_col.Lane])
 
         pie_data, textarea_fraction = create_pie_chart(known_run, unknown_run)
 
