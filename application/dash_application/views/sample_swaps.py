0c24044394319c5c00ca477e59346f77105dbbe8
Savo Lazic <savo.lazic@oicr.on.ca>
2021-04-29 10:32:03 -0400

    New sample swap algorithm to get around false positive swap calls

--- application/dash_application/views/sample_swaps.py
+++ application/dash_application/views/sample_swaps.py
@@ -2,8 +2,9 @@ import dash_core_components as core
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_table
-import gsiqcetl.picard.crosscheckfingerprints.utility
 import pinery
+import numpy
+import pandas
 
 from ..dash_id import init_ids
 from ..utility import df_manipulation, sidebar_utils
@@ -15,8 +16,9 @@ RUN_COLS = pinery.column.RunsColumn
 page_name = "sample_swaps"
 title = "Sample Swaps"
 
-# This cutoff should be very conservative to make sure all shown swaps are real
-LOD_CUTOFF = 100
+# LOD scores around 0 mean not enough data to determine swap
+# Set a zone left and right of 0 that where swaps will be ignored
+AMBIGUOUS_ZONE = 20
 
 ids = init_ids([
     # Buttons
@@ -31,24 +33,17 @@ ids = init_ids([
 ])
 
 special_cols = {
-    "latest_run": "LATEST_RUN"
+    "latest_run": "LATEST_RUN",
+    "same_identity": "SAME_IDENTITY",
+    "closest_libraries": "CLOSEST_LIBRARIES",
+    "closest_libraries_count": "CLOSEST_LIBRARIES_COUNT",
 }
 
-fingerprints = df_manipulation.get_crosscheckfingerprints()
-fingerprints[COL.Result] = gsiqcetl.picard.crosscheckfingerprints.utility.calculate_results(
-    fingerprints, LOD_CUTOFF, LOD_CUTOFF
-)
-
-# Picard uses LOD Score as truth ("UNEXPECTED" refers to the library pair)
-# Instead use library pair as truth ("UNEXPECTED" refers to LOD score that does not
-# match library identities)
-fingerprints[COL.Result] = fingerprints[COL.Result].replace({
-    "UNEXPECTED_MATCH": "UNEXPECTED_MISMATCH",
-    "UNEXPECTED_MISMATCH": "UNEXPECTED_MATCH",
-})
-
-# This pulls out swaps
-swap = fingerprints[fingerprints[COL.Result].str.startswith("UNEXPECTED")].copy()
+swap = df_manipulation.get_crosscheckfingerprints()
+swap = swap[~swap[COL.LibraryLeft].str.startswith("GSICAPBENCH")]
+swap = swap[~swap[COL.LibraryLeft].str.startswith("GLCS")]
+# The below algorithm won't work if libraries are compared with themselves
+swap = swap[swap[COL.LibraryLeft] != swap[COL.LibraryRight]]
 
 pinery_samples = df_manipulation.get_pinery_samples()
 swap = df_manipulation.df_with_pinery_samples_ius(
@@ -60,25 +55,59 @@ swap = df_manipulation.df_with_pinery_samples_ius(
 swap = df_manipulation.df_with_run_info(swap, COL.RunLeft)
 swap = df_manipulation.df_with_run_info(swap, COL.RunRight, "_RIGHT")
 
-# Pair-wise comparison is done both ways. Remove one of them to decrease redundant info
-swap["PAIR"] = swap.apply(
-    lambda x: tuple(sorted([x[COL.GroupValueLeft], x[COL.GroupValueRight]])), axis=1
-)
-swap = swap.drop_duplicates("PAIR")
-# Exclude OICR control libraries
-swap = swap[~swap[COL.LibraryLeft].str.startswith("GSICAPBENCH")]
-swap = swap[~swap[COL.LibraryLeft].str.startswith("GLCS")]
 # Get the latest run of the pair for sorting purposes
 swap[special_cols["latest_run"]] = swap[
     [RUN_COLS.StartDate, RUN_COLS.StartDate + "_RIGHT"]
 ].max(1)
+swap[special_cols["same_identity"]] = (
+    swap[PINERY_COL.RootSampleName] == swap[PINERY_COL.RootSampleName + "_RIGHT"]
+)
+
+
+def closest_lib(input_df):
+    """
+    The `input_df` must be sored by LOD_SCORE (descending) and have one library
+    in the LEFT_LIBRARY column. Returns the row with the most similar library and
+    adds a column that list the most similar libraries that had to be traversed until
+    one from the same patient was identified. If there was no swap, only one library
+    should have been traversed.
+    """
+    # Index will be used with `iloc` call, so has to go from 0 to number of rows
+    lib_df = input_df.reset_index(drop=True)
+    # The closest library
+    return_df = lib_df.head(1).copy()
+    # Libraries that came from the same patient
+    same_ident_df = lib_df[lib_df[special_cols["same_identity"]]]
+    if same_ident_df.empty:
+        return_df[special_cols["closest_libraries_count"]] = 0
+        return_df[special_cols["closest_libraries"]] = numpy.nan
+        return return_df
+
+    # Get libraries up to and including the first that came from the same patient
+    closest_df = lib_df.iloc[:same_ident_df.index[0] + 1]
+    return_df[special_cols["closest_libraries_count"]] = len(closest_df)
+    return_df[special_cols["closest_libraries"]] = ",".join(closest_df["RIGHT_LIBRARY"])
+    return return_df
+
+
+result = []
+# Two for loops (blah) go through each swap workflow and then check each library
+# Will try to vectorize this if this approach proves superior to previous hard LOD cutoff
+for _, top in swap.groupby(COL.FileSWID):
+    top = top.sort_values([COL.LibraryLeft, COL.LODScore], ascending=False)
+    for _, d in top.groupby(COL.LibraryLeft):
+        result.append(closest_lib(d))
+
+swap = pandas.concat(result)
+swap = swap[swap[COL.LODScore].abs() > AMBIGUOUS_ZONE]
+swap = swap[swap[special_cols["closest_libraries_count"]] > 1]
 
 COLUMNS_TO_SHOW = [
     COL.LibraryLeft,
     COL.LibraryRight,
-    special_cols["latest_run"],
-    COL.Result,
     COL.LODScore,
+    special_cols["latest_run"],
+    special_cols["closest_libraries"],
 ]
 
 # Pair-wise comparison is done within project (for now), so left project is sufficient
@@ -122,6 +151,7 @@ def layout(query_string):
                         sort_by=[{"column_id": "LATEST_RUN", "direction": "desc"}],
                         export_format="csv",
                         include_headers_on_copy_paste=True,
+                        style_cell={'textAlign': 'left', 'padding-right': '50px'},
                         style_data_conditional=[
                             {
                                 "if": {"row_index": "odd"},
