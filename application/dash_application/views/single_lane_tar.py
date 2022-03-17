85281fc7ae5ef451d6a03d3388963815b81ec7ba
Savo Lazic <savo.lazic@oicr.on.ca>
2022-03-17 14:52:01 -0400

    Turned on dnaSeqQC in single lane TAR and WG

--- application/dash_application/views/single_lane_tar.py
+++ application/dash_application/views/single_lane_tar.py
@@ -93,7 +93,7 @@ initial["cutoff_insert_median"] = 150
 
 
 def get_bamqc_data():
-    bamqc_df = util.get_bamqc3_and_4()
+    bamqc_df = util.get_dnaseqqc_and_bamqc4()
     bamqc_df = util.df_with_fastqc_data(bamqc_df, [BAMQC_COL.Run, BAMQC_COL.Lane, BAMQC_COL.Barcodes])
 
     bamqc_df[special_cols["Total Reads (Passed Filter)"]] = round(
