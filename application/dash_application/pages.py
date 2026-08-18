1746293fc517321a5d9c78292caf3c97c601ed42
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-08-18 15:07:37 -0400

    not sure why dev worked while this was broken

--- application/dash_application/pages.py
+++ application/dash_application/pages.py
@@ -8,35 +8,6 @@ from dash import html
 
 from .utility import sidebar_utils
 
-# Get the installed pages
-all_reports = []
-_report_sources = os.getenv("REPORT_SOURCES")
-for source in _report_sources.split(","):
-    sys.path.append(source)
-    for (dirpath, _, filenames) in os.walk(source):
-        for filename in filenames:
-            if filename.endswith(".py"):
-                module = importlib.import_module(os.path.join(dirpath, filename))
-                all_reports.append(getattr(module, "page_name").replace("-", "_"))
-
-_enabled_env = os.getenv("ENABLED_REPORTS")
-if _enabled_env:
-    _requested = [n.strip() for n in _enabled_env.split(",") if n.strip()]
-    for _name in _requested:
-        if _name not in all_reports:
-            print("Unknown report name in ENABLED_REPORTS: " + _name, file=sys.stderr)
-    pagenames = [n for n in _requested if n in all_reports]
-else:
-    pagenames = all_reports
-
-# Emulates the module members that are called in known_pages_router in case of error
-ErrorPage = namedtuple('ErrorPage', 'layout title dataversion page_name init_callbacks')
-
-# Please do not edit this array
-# https://media2.giphy.com/media/DkaZuJGcwwN32/200.webp?cid=790b761109288a37049d763e1175d0e4ca6307eee3351333&rid=200.webp
-pages = []
-
-
 def error_div(module_name, jira_error_summary, jira_error_text):
     """
     This emulates the `layout` function from the Dashi views. It displays the error
@@ -65,22 +36,43 @@ def error_div(module_name, jira_error_summary, jira_error_text):
 
     return f
 
+# Get the installed pages
+all_reports = []
+pages = []
+_report_sources = os.getenv("REPORT_SOURCES")
 
-# Please do not edit this loop
-for name in pagenames:
-    try:
-        pages.append(importlib.import_module(prefix + name))
-    except (IOError, OSError):
-        exception = traceback.format_exc()
-        jira_summary = "Error loading cache in " + name + " Dashi view"
-        print(exception, file=sys.stderr)
-        pages.append(ErrorPage(
-            error_div(name, jira_summary, exception),
-            name,
-            lambda: "Unknown version",
-            name,
-            lambda x: None,
-        ))
+# Emulates the module members that are called in known_pages_router in case of error
+ErrorPage = namedtuple('ErrorPage', 'layout title dataversion page_name init_callbacks')
 
+for source in _report_sources.split(","):
+    sys.path.append(source)
+    for (dirpath, _, filenames) in os.walk(source):
+        for filename in filenames:
+            if filename.endswith(".py"):
+                try:
+                    module = importlib.import_module(os.path.join(dirpath, filename))
+                    name = getattr(module, "page_name").replace("-", "_")
+                    all_reports.append(name)
+                    pages.append(module)
+                except (IOError, OSError):
+                    exception = traceback.format_exc()
+                    jira_summary = "Error loading cache in " + name + " Dashi view"
+                    print(exception, file=sys.stderr)
+                    pages.append(ErrorPage(
+                        error_div(name, jira_summary, exception),
+                        name,
+                        lambda: "Unknown version",
+                        name,
+                        lambda x: None,
+                    ))
 
-# TODO: Maybe move the user-defined array to another file and import it into a scarier-sounding file
+_enabled_env = os.getenv("ENABLED_REPORTS")
+if _enabled_env:
+    _requested = [n.strip() for n in _enabled_env.split(",") if n.strip()]
+    for _name in _requested:
+        if _name not in all_reports:
+            print("Unknown report name in ENABLED_REPORTS: " + _name, file=sys.stderr)
+    pagenames = [n for n in _requested if n in all_reports]
+else:
+    pagenames = all_reports
+
